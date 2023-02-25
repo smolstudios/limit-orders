a6f18bf101813e289dcd97952b44cf97b74ccd61
John Johnson <johnrjj@gmail.com>
2023-02-25 14:51:00 -0500

    feat: dynamic page route slug

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -1,16 +1,16 @@
-// "use client";
-
-import Image from "next/image";
-import { FaGithub } from "react-icons/fa";
-
-
-export default function Home() {
+export default function Page({
+  params,
+}: {
+  params: { slug: string[] | undefined };
+}) {
   return (
     <div>
       <div className="relative z-10 flex h-screen w-full items-center justify-center">
         <div className="flex flex-col items-start p-4">
           <p className="mb-6 text-8xl font-medium text-white">
-            wagmi 
+            {params.slug?.map((s) => {
+              return <span key={s}> {s} </span>;
+            })}
           </p>
         </div>
       </div>
