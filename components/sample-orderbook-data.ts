c1ba88e3e5838e3d922e38ae36d89f2a98b5848b
John Johnson <johnrjj@gmail.com>
2023-02-25 17:10:42 -0500

    feat: orderbook depth chart

---
+++ components/sample-orderbook-data.ts
@@ -0,0 +1,186 @@
+export const orderBook = {
+    "name": "BTCUSDT",
+    "market": "BTC/USDT",
+    "bids": [{
+        "side": "buy",
+        "price": 9360.84809315,
+        "amount": 9.8,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9375.1,
+        "amount": 9.75,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9382.62,
+        "amount": 9.5,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9387.960548,
+        "amount": 9.25,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9396.314,
+        "amount": 9,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9397.89591763,
+        "amount": 8.5,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9398.2,
+        "amount": 8,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9399,
+        "amount": 7.5,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9400,
+        "amount": 7,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9419.00000032,
+        "amount": 6.5,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9430.98991003,
+        "amount": 6,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9440.10101101,
+        "amount": 5,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9470.1,
+        "amount": 4,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9490.0005,
+        "amount": 2,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }, {
+        "side": "buy",
+        "price": 9500.00000005,
+        "amount": 1,
+        "timestamp": 1512117867567,
+        "datetime": "Dec 5 2017, 01:44:27:567 AM"
+    }],
+    "asks": [{
+        "side": "sell",
+        "price": 9510.00000003,
+        "amount": 1,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9551.95399999,
+        "amount": 2,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9572.95399999,
+        "amount": 4,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9595,
+        "amount": 5,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9622,
+        "amount": 6,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9647.99999998,
+        "amount": 6.5,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9698,
+        "amount": 7,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9699,
+        "amount": 7.25,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9699.00000009,
+        "amount": 8,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9700,
+        "amount": 9.3,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9700.64999999,
+        "amount": 9.7,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9700.65000002,
+        "amount": 9.9,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9710,
+        "amount": 10,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9719.99999999,
+        "amount": 10.1,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }, {
+        "side": "sell",
+        "price": 9729,
+        "amount": 10.4,
+        "timestamp": 1512117867566,
+        "datetime": "Dec 5 2017, 01:44:27:566 AM"
+    }]
+};
